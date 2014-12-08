b0811e34bfd922902d924672171eefefa3b1afdf
Trevor Joynson (trevorj) <github@skywww.net>
2014-12-08 17:20:48 -0500

    Add backtothefuture

---
+++ plugins/backtothefuture.py
@@ -0,0 +1,19 @@
+"""!backtothefuture <date> returns the delorean dashboard with the specified <date>"""
+
+import re
+import parsedatetime
+
+
+p = parsedatetime.Calendar()
+
+
+def backtothefutureday(datestr):
+    dt = p.parse(datestr)
+    return 'http://www.itsbacktothefutureday.com/images/{year}/{month}/{day}.jpg'.format(year=dt.tm_year, month=dt.tm_mon, day=dt.tm_mday)
+
+
+def on_message(msg, server):
+    text = msg.get("text", "")
+    match = re.findall(r"!backtothefuture (.*)", text)
+    if match:
+        return backtothefutureday(match[0])
