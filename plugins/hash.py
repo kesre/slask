d453beef995dd01d475635b4e4315fa04b0ac091
Jeff Hemphill <jhemphill@fb.com>
2015-01-31 01:48:17 -0800

    Rename module to prevent naming conflict

--- plugins/hash.py
+++ plugins/hash.py
@@ -4,8 +4,18 @@ import md5
 import re
 
 def on_message(msg, server):
+    """
+
+    >>> on_message({"text": "!md5 asdf"}, None)
+    '912ec803b2ce49e4a541068d495ab570'
+
+    """
     text = msg.get("text", "")
     match = re.findall(r"!md5 (.*)", text)
     if not match: return
 
     return md5.md5(match[0]).hexdigest()
+
+
+if __name__ == "__main__":
+    import doctest; doctest.testmod()
