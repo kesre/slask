024d9bd00d4459d8d169fa2007eff69d7855fefd
joshshadowfax <joshshadowfax@gmail.com>
2015-01-31 01:05:56 -0800

    Fixing errors and adding perfect match preference

--- plugins/magic.py
+++ plugins/magic.py
@@ -1,23 +1,41 @@
 """!magic <search term> return the first magic card with name containing <search term>"""
 
 import re
+import random
 from urllib import quote
 
 import requests
 
 def magic(searchterm):
-    searchterm = quote(searchterm)
+    quotedsearchterm = quote(searchterm)
     url = "https://api.deckbrew.com/mtg/cards?name={}"
-    url = url.format(searchterm)
+    url = url.format(quotedsearchterm)
 
-    j = requests.get(url).json()
+    results = requests.get(url).json()
 
     if not results:
         return "no cards found"
 
-    card = results[0]["editions"][0]["image_url"]
+    edition = {}
 
-    return card
+    for card in results:
+        if searchterm.lower() == card["name"].lower():
+            #exact match found, pick a random edition with valid image
+            return getimage(card)
+        
+    #no exact match found, pick a random card from the matches
+    card = random.choice(results)
+    return getimage(card)
+
+def getimage(editions):
+    valideditions = [i for i in editions["editions"] if hasimage(i["image_url"])]
+    if not valideditions:
+        return "no image available"
+    edition = random.choice(valideditions)
+    return edition["image_url"]
+
+def hasimage(url):
+    return url != "https://image.deckbrew.com/mtg/multiverseid/0.jpg"
 
 def on_message(msg, server):
     text = msg.get("text", "")
